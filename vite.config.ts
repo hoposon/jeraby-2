979cda53bbdd7a6d4ee4272c1a94513781d924e8
Lukáš Houf <hoposon@gmail.com>
2023-09-22 15:56:18 +0200

    localizations fix

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,9 @@ export default defineConfig({
           // generate localizations files to separare dir
           let packageName = ''
           if (id.includes('localizations/loc')) {
-            console.log('🚀 ~ file: vite.config.ts:25 ~ id:', id)
             const matcher = new RegExp(`localizations/loc/.*`)
             const match = id.match(matcher)
-            console.log('🚀 ~ file: vite.config.ts:28 ~ match:', match)
             if (match && match[0]) {
-              // packageName = match[0].slice(match[0].indexOf('/')+1,-1)+'/'+
               const splitted = match[0].split('/')
               if (splitted.length === 4) {
                 packageName = 'loc/'+splitted[2]+'/loc'
@@ -35,19 +32,6 @@ export default defineConfig({
               } 
             }            
           }
-          // // generate localizations files to separare dir
-          // let packageName = ''
-          // if (id.includes('common/localizations')) {
-          //   const match = id.match(/common\/localizations\/.*/)            
-          //   if (match && match[0]) {
-          //     // packageName = match[0].slice(match[0].indexOf('/')+1,-1)+'/'+
-          //     const splitted = match[0].split('/')
-          //     if (splitted.length === 5) {
-          //       packageName = 'localizations/'+splitted[2]+'/'+splitted[3]+'/'+splitted[2]
-          //       return packageName
-          //     }
-          //   }            
-          // }
         },
       }
     }
