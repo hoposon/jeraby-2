c19d618390398dad0ecee1009a6abc1d6f297d08
Lukáš Houf <hoposon@gmail.com>
2024-08-03 11:54:16 +0200

    add /cs /en to cors

--- functions/index.ts
+++ functions/index.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 const appi = express();
 appi.use(bodyParser.json());
 
-const whitelist = ['https://michaelahouf.cz', 'https://jeraby-84a7e.web.app']
+const whitelist = ['https://michaelahouf.cz', 'https://jeraby-84a7e.web.app', 'https://michaelahouf.cz/cs', 'https://jeraby-84a7e.web.app/en']
 const corsOptions: CorsOptions = {
   origin: function (origin: string | undefined, callback: (err: Error|null, origin?: boolean | string | RegExp | (boolean | string | RegExp)[]) => void) {
     console.log('🚀 ~ origin:', origin)
